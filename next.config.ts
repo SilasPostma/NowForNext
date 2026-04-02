ac5a5ca030305693e697581b545de37e03e781e5
Silas Postma <silas.postma@student.uva.nl>
2026-04-02 13:45:52 -0700

    ik snap t niet

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const isGithubPages = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  basePath: isGithubPages ? "/NowForNext" : "",
-  assetPrefix: isGithubPages ? "/NowForNext/" : "",
-  reactCompiler: true,
+  basePath: "",
+  assetPrefix: "",
 };
 
 export default nextConfig;
