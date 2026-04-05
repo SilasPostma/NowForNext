c474995f1a28498bd8401810e2cffb6aa4af8600
Silas Postma <silas.postma@student.uva.nl>
2026-04-05 11:23:13 -0700

    fix header dissapearing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/ui/header";
 
 export default function HomePage() {
   return (
-    <main className="w-full h-screen">
+    <main className="w-full">
       <Header />
       <Panels />
     </main>
